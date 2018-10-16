57439ffadec3cfb520d13fb41f584c9cde20a761
Dorina Dumbrava <drndumbrava@gmail.com>
2018-10-16 17:22:58 +0300

    'added the input Parser'

---
+++ TrimesterReport/EmployeeDataParser.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrimesterReport
+{
+    class EmployeeDataParser
+    {
+        private readonly int nrOfarguments = 5;
+        public void ParseData(string employeeInput)
+        {
+            string[] employeeData = employeeInput.Split(' ');
+            if (employeeData.Length == nrOfarguments)
+            {
+
+            }
+        }
+    }
+}
--- TrimesterReport/Program.cs
+++ TrimesterReport/Program.cs
@@ -10,12 +10,82 @@ namespace TrimesterReport
     {
         static void Main(string[] args)
         {
-            // The code provided will print ‘Hello World’ to the console.
-            // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
-            Console.WriteLine("Hello World!");
-            Console.ReadKey();
+            Console.WriteLine("Chose a working method");
+            Console.WriteLine("Write 'F' to read from file, 'I' to pass arguments and pres Enter to to read from console");
+            string userChoice = Console.ReadLine().ToUpper();
 
-            // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
+            EmployeeDataParser input1 = new EmployeeDataParser();
+            switch (userChoice)
+            {
+                case "F":
+                    {
+                        Console.WriteLine("Give a path to your file");
+                        //validate the input
+                        string connectToUserInput = Console.ReadLine();
+                        string allUserInputs = System.IO.File.ReadAllText(connectToUserInput);
+                        string[] fileUserInputs = allUserInputs.Split(';');
+                        if (fileUserInputs.Length > 0)
+                        {
+                            foreach (string input in fileUserInputs)
+                            {
+                                input1.ParseData(input);
+                            }
+                        }
+                        else
+                        {
+                            Console.WriteLine("There are no arguments inserted");
+                        }
+                        //EmployeeData.CreateUser();
+                        //GetData.ShowUserInputs();
+                    }
+                    break;
+                case "I":
+                    {
+                        Console.WriteLine("The data will be taken from arguments");
+                        if (args.Length > 0)
+                        {
+                            foreach (string input in args)
+                            {
+                                string[] argsUserInputs = input.Split(';');
+                                if (argsUserInputs.Length > 0)
+                                {
+                                    foreach (string cmdInput in argsUserInputs)
+                                    {
+                                        input1.ParseData(cmdInput);
+                                    }
+                                }
+                            }
+                        }
+                        else
+                        {
+                            Console.WriteLine("There are no arguments inserted");
+                        }
+                        //EmployeeData.CreateUser();
+                        //GetData.ShowUserInputs();
+                    }
+                    break;
+                case "":
+                    {
+                        Console.WriteLine("Write your user data: ");
+                        Console.WriteLine("Once you finsh your inputs, press Enter.");
+                        string consoleInput = " ";
+                        while (consoleInput.Length > 0)
+                        {
+                            consoleInput = Console.ReadLine();
+                            //GetData.ReadAndSplit(consoleInput);
+                        }
+                        //EmployeeData.CreateUser();
+                        //GetData.ShowUserInputs();
+
+                    }
+                    break;
+                default:
+                    break;
+            }
+            Console.WriteLine("");
+            Console.WriteLine("Your users:");
+            //EmployeeData.Show();
+            Console.ReadKey();
         }
     }
 }
